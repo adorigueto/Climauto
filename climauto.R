0181a725f705416497edbce767de7707b3cae10b
André Canal <93547260+adorigueto@users.noreply.github.com>
2022-07-08 14:58:48 -0300

    tried new approaches

--- climauto.R
+++ climauto.R
@@ -7,14 +7,17 @@ library(ggplot2)
 
 # Read file
 climauto_raw <- read.csv("Climauto.csv")
-climauto <- data.frame(climauto_raw)
+climauto_df <- data.frame(climauto_raw)
+
+# Remove sundays from the data frame
+climauto <- climauto_df[climauto_df$dia_da_semana != "dom",]
 
 # Set variables
 quantity <- climauto$quantidade
-model <- climauto$modelo
+type <- climauto$modelo
 week_day <- factor(climauto$dia_da_semana,
-    levels = c("dom", "seg", "ter", "qua", "qui", "sex", "sab"),
-    labels = c("dom", "seg", "ter", "qua", "qui", "sex", "sab"),
+    levels = c("seg", "ter", "qua", "qui", "sex", "sab"),
+    labels = c("seg", "ter", "qua", "qui", "sex", "sab"),
     ordered = TRUE)
 month <- factor(climauto$mes,
     levels = c("jan", "fev", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out", "nov", "dez"),
@@ -29,11 +32,27 @@ plot(week_day, quantity)
 plot(month, quantity)
 plot(day, quantity)
 
-model_test <- aov(quantity ~ week_day+month)
+model_test <- aov(quantity ~ week_day + month)
 summary(model_test)
 qqnorm(residuals(model_test))
 qqline(residuals(model_test))
 shapiro.test(residuals(model_test))
 
-ggplot(climauto, aes(day, quantity)) +
-    geom_point()
+monday <- climauto[climauto$dia_da_semana == "seg",]$quantidade
+tuesday <- climauto[climauto$dia_da_semana == "ter",]$quantidade
+wednesday <- climauto[climauto$dia_da_semana == "qua",]$quantidade
+thursday <- climauto[climauto$dia_da_semana == "qui",]$quantidade
+friday <- climauto[climauto$dia_da_semana == "sex",]$quantidade
+saturday <- climauto[climauto$dia_da_semana == "sab",]$quantidade
+saturday <- saturday[-c(53)]
+
+week_days <- data.frame(monday, tuesday, wednesday, thursday, friday, saturday)
+week_days
+week_days_mean <- colMeans(week_days, na.rm = TRUE)
+week_days_mean
+week_days_sum <- colSums(week_days, na.rm = TRUE)
+week_days_sum
+
+week_day <- data.frame(week_days_mean, week_days_sum)
+plot(week_day$week_days_mean)
+plot(week_day$week_days_sum)
